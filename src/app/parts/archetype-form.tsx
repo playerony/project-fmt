2b40b8e374f3d598d29fbd212a33afbed9f3dae9
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2024-01-15 18:51:48 +0100

    feat: create archetype form page (#16)

--- src/app/parts/archetype-form.tsx
+++ src/app/parts/archetype-form.tsx
@@ -1,3 +1,4 @@
+import { Icons } from '@/components/icons';
 import { RadioGroupController } from '@/components/radio-group-controller';
 import { Button } from '@/components/ui/button';
 import {
@@ -13,9 +14,10 @@ import { Control } from 'react-hook-form';
 
 interface StoryFormProps {
   control: Control<{ archetype?: string }>;
+  selectedArchetype: string | undefined;
 }
 
-export const ArchetypeForm = ({ control }: StoryFormProps) => (
+export const ArchetypeForm = ({ control, selectedArchetype }: StoryFormProps) => (
   <div className="mx-auto flex h-full w-full flex-col justify-center space-y-6 sm:w-[450px]">
     <Card>
       <CardHeader>
@@ -37,31 +39,37 @@ export const ArchetypeForm = ({ control }: StoryFormProps) => (
               label: 'Coming of age',
               value: 'coming-of-age',
               itemClassName: 'peer sr-only',
+              icon: <Icons.comingOfAgeArchetype className="h-[75px] w-[75px]" />,
             },
             {
               label: 'Overcoming Obstacles',
               value: 'overcoming-obstacles',
               itemClassName: 'peer sr-only',
+              icon: <Icons.overcomingObstaclesArchetype className="h-[75px] w-[75px]" />,
             },
             {
               label: 'Constant Evolution',
               value: 'constant-evolution',
               itemClassName: 'peer sr-only',
+              icon: <Icons.constantEvolutionArchetype className="h-[75px] w-[75px]" />,
             },
             {
               label: 'True As It Ever Was',
               value: 'true-as-it-ever-was',
               itemClassName: 'peer sr-only',
+              icon: <Icons.trueAsItEverWasArchetype className="h-[75px] w-[75px]" />,
             },
             {
               label: 'Rebirth',
               value: 'rebirth',
+              icon: <Icons.rebirthArchetype className="h-[75px] w-[75px]" />,
               itemClassName: 'peer sr-only',
             },
             {
               label: 'Quest',
               value: 'quest',
               itemClassName: 'peer sr-only',
+              icon: <Icons.questArchetype className="h-[75px] w-[75px]" />,
             },
           ]}
           renderOptionChildren={(option) => (
@@ -69,10 +77,57 @@ export const ArchetypeForm = ({ control }: StoryFormProps) => (
               className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
               htmlFor={option.value}
             >
+              {option.icon}
               {option.label}
             </Label>
           )}
         />
+        {selectedArchetype === 'coming-of-age' ? (
+          <p className="mb-4 text-sm text-muted-foreground">
+            After years of learning and growing, you’ve reached a milestone. You know who you are.
+            You are strong, experienced, and confident. You’re ready to take on whatever comes next.{' '}
+            <strong>Use this archetype to show experience and strength.</strong>
+          </p>
+        ) : null}
+        {selectedArchetype === 'overcoming-obstacles' ? (
+          <p className="mb-4 text-sm text-muted-foreground">
+            You hit a roadblock and almost failed. It was a struggle. You challenged your
+            assumptions, made tough decisions, fought hard… and won.{' '}
+            <strong>Use this archetype to demonstrate resilience.</strong>
+          </p>
+        ) : null}
+        {selectedArchetype === 'constant-evolution' ? (
+          <p className="mb-4 text-sm text-muted-foreground">
+            You never rest. Over and over, you’ve evolved to handle whatever comes your way. You
+            keep moving, adapting to the world around you and maybe even anticipating its change.{' '}
+            <strong>Use this archetype to prove adaptability.</strong>
+          </p>
+        ) : null}
+        {selectedArchetype === 'true-as-it-ever-was' ? (
+          <p className="mb-4 text-sm text-muted-foreground">
+            Your purpose and values have endured throughout the years. The world has evolved and
+            you’ve changed with it, but your core beliefs have remained intact. They guide
+            everything you do.{' '}
+            <strong>
+              Use this archetype to communicate steadiness or reinforce the audience’s trust.
+            </strong>
+          </p>
+        ) : null}
+        {selectedArchetype === 'rebirth' ? (
+          <p className="mb-4 text-sm text-muted-foreground">
+            Over time, you’ve accumulated a wealth of experience and knowledge. Today, you’re
+            harnessing that to start a whole new chapter, and become an even better version of
+            yourself. <strong>Use this archetype to explain a new direction.</strong>
+          </p>
+        ) : null}
+        {selectedArchetype === 'quest' ? (
+          <p className="mb-4 text-sm text-muted-foreground">
+            You’ve always had a clear objective and you’ve pursued it relentlessly. Over time,
+            you’ve summoned your talent and passion to turn your vision into reality. Through twists
+            and turns, you’ve maintained your focus, and you will never let it go.{' '}
+            <strong>Use these archetype to recommit or demonstrate dedication.</strong>
+          </p>
+        ) : null}
       </CardContent>
       <CardFooter>
         <Button className="w-full">Continue</Button>
---
+++ src/components/icons.tsx
@@ -0,0 +1,444 @@
+import { HTMLAttributes } from 'react';
+
+type IconProps = HTMLAttributes<SVGElement>;
+
+export const Icons = {
+  comingOfAgeArchetype: (props: IconProps) => (
+    <svg fill="none" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M165.865 345C67.4268 338.855 27.5031 77.085 213.503 69.1662C405.382 60.997 340.806 357.21 197.786 260.179C147.022 225.723 192.405 137.4 241.736 158.785"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+      </g>
+    </svg>
+  ),
+  overcomingObstaclesArchetype: (props: IconProps) => (
+    <svg fill="none" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M41 280C46.1372 271.746 49.6326 262.651 55.0169 254.565C62.361 243.535 68.375 233.297 77.7939 223.867C96.8047 204.834 128.166 194.675 146.124 220.359C153.625 231.088 153.207 245.731 163.207 255.442C171.367 263.367 193.541 270.296 204.38 257.635C221.025 238.193 241.757 120.007 282.785 140.546C321.371 159.862 305.235 237.417 335.785 268.004C342.618 274.844 348.914 275.897 359 277.651"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M246.819 140.446C247.788 141.724 250.523 141.866 251.473 142.953C251.588 143.083 244.857 153.871 244.218 154.105C243.188 154.482 239.483 153.195 238.742 152.601C236.681 150.951 238.26 149.628 239.563 147.84C241.16 145.648 242.357 143.252 243.944 141.073"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M234.85 157.901C236.324 158.999 240.092 158.072 241.175 159.974C241.376 160.327 235.755 173.559 235.694 173.598C235.321 173.834 229.841 172.021 229.651 171.821C229.126 171.268 228.36 169.763 228.668 169.008C229.624 166.654 230.96 164.451 231.899 162.047C232.548 160.389 233.18 159.033 234.148 157.604"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M262.679 128.989C263.315 129.838 267.18 132.423 267.269 133.322C267.321 133.853 266.77 134.078 266.434 134.306C264.341 135.725 259.36 140.85 256.629 140.995C254.095 141.131 250.243 134.566 252.613 133.028C254.94 131.517 257.62 130.45 259.778 128.734"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M238.988 134.222C243.647 140.362 230.874 138.672 237.316 133.583"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M231.053 144.213C231.79 143.707 232.863 143.097 233.826 143.621C234.103 143.772 234.332 144.943 234.316 145.101C233.815 150.097 225.038 145.428 230.238 142.734"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M224.452 154.336C224.892 153.526 225.661 152.455 226.813 152.606C229.496 152.957 222.878 159.714 222.878 154.48C222.878 153.215 223.246 152.438 224.452 151.885"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M224.022 162.87C220.448 165.273 218.967 164.965 219.581 161.946C220.195 158.928 221.675 159.236 224.022 162.87Z"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M257.581 121.446C264.997 127.31 247.743 126.72 256.113 121"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M249.363 126.719C253.854 131.483 240.857 133.874 247.886 127.085"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="12"
+        ></path>
+        <path
+          d="M277.784 139.302C277.199 152.783 276.886 166.449 276.302 179.998C275.448 199.792 274.699 219.607 273.633 239.391C273.141 248.511 272.15 257.469 272.15 266.613C272.15 268.124 271.825 278.541 272.447 276.237"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M107.346 211.367C107.177 233.875 106.202 256.596 106.202 278.856"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M257.194 149.597C256.522 191.519 251.475 233.393 251.475 275.425"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M234.317 204.503C231.824 227.416 234.334 253.195 233.339 275.424"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M216.014 249.115C215.837 257.27 214.871 268.434 214.871 276.568"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M294.943 159.892C293.638 167.28 293.926 174.779 293.926 182.262C293.926 202.783 292.655 223.258 292.655 243.777C292.655 250.513 292.655 266.43 292.655 273.137"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M309.078 233.101C308.615 247.347 310.195 261.926 309.078 274.28"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M196.568 270.849C196.193 273.517 195.79 276.2 195.424 278.856"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M178.443 271.993C178.442 273.789 178.092 279.145 178.092 277.349"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M161.204 261.698C160.925 267.023 161.028 272.374 161.028 277.712"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M140.605 218.23C139.993 237.291 142.806 256.602 142.806 275.425"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M124.119 212.511C122.593 227.152 124.1 272.299 125.527 277.712"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M92.4748 226.237C91.1002 243.373 90.187 260.538 90.187 277.712"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M75.6725 247.971C75.3567 256.874 76.464 269.593 74.874 276.568"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M60.8718 268.561C59.3939 272.004 60.341 275.401 60.0733 278.856"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+        <path
+          d="M326.226 269.705C325.958 271.229 325.693 272.755 325.425 274.281"
+          opacity="0.503384"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="4"
+        ></path>
+      </g>
+    </svg>
+  ),
+  constantEvolutionArchetype: (props: IconProps) => (
+    <svg fill="none" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M163.142 89.9557C176.467 72.7332 193.422 60.0556 216.662 65.1298C282.155 79.4264 269.557 180.344 204.77 184.889C152.311 188.572 132.684 99.5168 175.034 87.035"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M150 210C152.97 237.009 155.808 260.269 158.515 279.781C161.222 299.293 163.384 320.366 165 343"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M248.235 216.822C245.811 230.515 239.568 259.88 234.87 286.947C229.675 316.88 226.117 344 226.117 344"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M149.83 208.816C142.124 209.354 74.168 225.283 71.3345 225.115C70.6641 225.072 70.0075 225.031 69.3398 224.977C68.0348 224.872 65.858 224.809 64.8454 224.428C62.7127 223.603 60.7925 222.959 59.1122 221.312C58.4125 220.63 60.9308 202.275 68.1742 172.505C74.8686 144.991 85.0504 120.92 87.1044 119.195C89.1322 117.504 103.116 106.233 103.985 106.233C105.467 106.233 106.937 106.544 108.433 106.765C110.662 107.103 116.396 111.668 117.378 112.885C119.425 115.441 117.985 118.799 114.324 124.188C110.662 129.577 102.428 129.577 99.487 132.006"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M250.235 211.219C265.262 211.219 336.816 239.868 338.172 241.884C339.593 244.006 341 245.917 341 248.358C341 252.658 337.853 256.598 334.121 259.782C328.918 264.216 323.011 268.174 317.383 272.315C314.063 274.752 310.732 277.2 307.411 279.637C302.181 283.479 258.811 314.848 253.101 318.249C249.094 320.64 244.989 322.987 241.308 325.643C241.233 326.191 241.087 326.742 240.876 327.285C240.397 328.489 234.561 341.928 233.647 343"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+      </g>
+    </svg>
+  ),
+  trueAsItEverWasArchetype: (props: IconProps) => (
+    <svg fill="none" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M168.443 163.289C178.503 159.811 192.721 158.072 211.097 158.072C238.662 158.072 249.178 156.044 255.821 158.072"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M211.564 160.842C208.697 199.775 207.263 225.881 207.263 239.158C207.263 259.074 205.891 312.321 205.891 317.475"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M126.455 230.592C135.014 262.547 147.708 285.462 164.537 299.339C189.781 320.154 197.672 321.034 221.307 312.086C244.943 303.137 280.159 268.682 284.189 249.637"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M108.301 258.737C111.722 255.441 123.64 226.921 126.962 226.921C130.285 226.921 142.878 233.64 153.692 248.345"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M261.497 262.185C274.83 247.848 282.305 241.475 283.921 243.068C286.346 245.457 291.001 260.394 291.001 268.527"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M193.879 95.8294C196.204 86.9599 202.128 82.5251 211.65 82.5251C225.934 82.5251 235.396 95.3767 235.396 104.768C235.396 114.16 226.92 136.368 214.637 136.368C202.354 136.368 185.561 127.07 187.968 109.446"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+      </g>
+    </svg>
+  ),
+  rebirthArchetype: (props: IconProps) => (
+    <svg
+      fill="none"
+      stroke="#ffffff"
+      viewBox="0 0 400 400"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M179.222 296.039C171.929 321.836 117.118 349.829 100.868 317.327C83.9601 283.513 128.707 258.291 155.272 253.461"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M154.089 255.235C140.063 256.692 126.178 245.826 114.468 239.268C74.9513 217.139 49.2305 174.06 54.7419 127.208C60.5666 77.6967 120.975 104.935 142.262 123.068C160.693 138.769 174.617 160.101 184.839 181.908C193.152 199.64 200.112 214.666 205.536 233.651"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M211.746 221.528C216.22 203.929 220.6 185.643 227.417 168.602C234.927 149.827 243.612 130.546 256.097 114.494C267.281 100.115 309.676 53.8821 331.79 73.9865C338.609 80.1856 341.437 92.7089 343.321 101.189C355.43 155.676 303.486 234.412 246.044 240.156"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M249.001 238.972C282.8 227.116 321.478 272.606 305.179 305.204C286.258 343.046 248.409 334.476 215.589 283.62"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M205.537 171.56C213.34 215.845 203.3 268.936 199.919 312.892"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+      </g>
+    </svg>
+  ),
+  questArchetype: (props: IconProps) => (
+    <svg fill="none" viewBox="0 0 400 400" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M136.321 95.7854C172.731 95.452 196.648 94.9459 208.073 94.2669C225.209 93.2484 263.404 92.4634 270.086 96.3145C276.767 100.166 296 119.797 296 123.105C296 126.414 275.024 145.445 273.677 146.525C272.329 147.605 267.998 149.969 263.344 149.969C258.69 149.969 140.626 153.263 134.497 149.969C128.369 146.674 132.159 115.464 132.159 112.517C132.159 109.571 132.159 106.373 132.159 104.151"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M264.679 172.285C228.269 172.285 204.352 171.945 192.927 171.267C175.791 170.249 137.596 169.464 130.914 173.313C124.233 177.163 105 196.287 105 199.594C105 202.9 125.976 223.423 127.323 224.502C128.671 225.582 133.002 227.945 137.656 227.945C142.31 227.945 260.374 229.739 266.503 226.446C272.631 223.153 268.841 191.955 268.841 189.01C268.841 186.064 268.841 182.868 268.841 180.647"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+        <path
+          d="M206 230C206.156 247.007 206.49 278.674 207 325M207 75C206.494 81.6777 206.16 88.3444 206 95L207 75ZM205.987 153C205.987 157.111 205.996 161.444 206.012 166L205.987 153Z"
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+        ></path>
+      </g>
+    </svg>
+  ),
+};
